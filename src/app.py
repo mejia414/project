4b6b98eae9c348d5de363ad17b3acd6467327419
mejia414 <mejia414@hotmail.com>
2020-05-18 16:54:05 -0500

    x com

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import jsonify
+from flask import render_template
+from flask import redirect
+from flask import Request
+from flask import Response
 
 app = Flask(__name__,)
 
 
 @app.route('/', methods=['GET'])
 def index():
+    x = 'hello world'
+    return render_template('index.html', x=x)
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
     return jsonify({'response': 'hello world'})
 
 
